9bff31e14c02c3188a599ac1d16c43cf6a4c1754
Wantonfury <orzan.alexandru94@gmail.com>
2022-07-30 19:17:22 +0300

    Add basic buttons and screen with no functionality

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+let screen = document.querySelector("#screen-text");
+
+let displayValue = 0;
+let storedValue = 0;
+
+let display = () => {
+    screen.textContent = displayValue;
+}
+
+let init = () => {
+    
+}
+
+init();
